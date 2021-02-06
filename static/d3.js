b083f47ffeb5235059ffa98a08237680c016e87e
your username <69278240+shadiaduery@users.noreply.github.com>
2021-02-06 13:18:23 -0800

    commit

--- static/d3.js
+++ static/d3.js
@@ -20,7 +20,7 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // either works!
-// url = "http://127.0.0.1:5000/pizzas"
+
 url = "http://127.0.0.1:5000/emission"
 
 d3.json(url).then(function(data) {
