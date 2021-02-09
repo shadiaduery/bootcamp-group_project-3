5a3bbaef8a11f341a05431797a1ec532c253107b
larunachalam10 <priyac863@gmail.com>
2021-02-08 20:26:02 -0800

    files saved

--- static/d3.js
+++ static/d3.js
@@ -36,17 +36,20 @@ function getScatterPlot(year) {
         console.log(year)
         // console.log("selecting data")
         var emission_population = []
-        emission_population = emission[year].Values
-        console.log(emission_population)
+        // emission_population = emission[year].Values
+        console.log(emission[])
 
         var emi = []
         var pop = []
         var city=[]
-        emission_population.forEach(function (data) {
+        emission.forEach(function (data) {
             emi.push(data.Emission)
             pop.push(data.Population)
             city.push(data.City)
         });
+        console.log(emi);
+        console.log(pop);
+        console.log(city);
         var trace1 = {
             x: pop,
             y: emi,
@@ -81,25 +84,25 @@ function optionChanged(year) {
 function init() {
     var dropdownMenu = d3.selectAll("#selDataset");
     d3.json(url).then(function (emission) {
-        console.log(emission)
-        console.log("xxxx")
-        console.log(emission[2011].Year)
-        console.log("xxxxxxxxx")
+        // console.log(emission)
+        // console.log("xxxx")
+        // console.log(emission[2011].Year)
+        // console.log("xxxxxxxxx")
         //var year_data = [emission[2010].Year, emission[2011].Year,emission[2012].Year,
         //                emission[2013].Year,emission[2014].Year,emission[2015].Year,
         //                emission[2016].Year,emission[2017].Year]
         
-        console.log("YYYYYYYYY")
-        console.log(year_data)
+        // console.log("YYYYYYYYY")
+        // console.log(year_data)
 
-        console.log("qwertyu")
+        // console.log("qwertyu")
         var year_data = [];
         for (let i = 2010; i < 2018 ; i++) {
             year_data.push(emission[i].Year)
-            console.log(emission[i].Year)
+            // console.log(emission[i].Year)
         }
-        console.log(year_data)
-        console.log("qwertyu")
+        // console.log(year_data)
+        // console.log("qwertyu")
         year_data.forEach(function (year) {
             dropdownMenu.append("option").text(year).property("value");
             //console.log(year_data[0])
