c30aaa2ddd5c9198e7631bc346c0112d39cdc5bb
Nataliia Sokolova <newbeadwork@gmail.com>
2021-02-08 21:10:24 -0800

    All 4 visuals

--- static/map.js
+++ static/map.js
@@ -38,7 +38,10 @@ d3.json(url).then(function(data) {
             "City_State": data[i].City_State,
             "City": data[i].City,
             "State": data[i].State,
-            "emissionsPerPopulation2017": +data[i].emissions2017/+data[i].Population2017,
+            "Population2010": +data[i].Population2010,
+            "Population2017": +data[i].Population2017,
+            "emissionsPerPopulation2010": (+data[i].emissions2010/+data[i].Population2010).toFixed(2),
+            "emissionsPerPopulation2017": (+data[i].emissions2017/+data[i].Population2017).toFixed(2),
             "percentDifferenceEmission": percentDifferenceEmission
           }
         });
@@ -47,21 +50,22 @@ d3.json(url).then(function(data) {
   
 //Function for pop-up
 function createPopups(feature, layer) {
-  layer.bindPopup("<h3>" + feature.properties.City_State +
-    //"</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
-    //"</h3><hr><p><strong>Magnitude: " + feature.properties.mag + "</strong></p>" +
-    "</h3><hr><p><strong>Difference in emissions per population between 2010 and 2017 (%):" + feature.properties.percentDifferenceEmission + "</strong></p>");
+  layer.bindPopup("<h5><strong>" + feature.properties.City_State + "</strong></h5>" + 
+    "<p>CO2 emissions per population, 2010: <strong>" + feature.properties.emissionsPerPopulation2010 + " kg</strong></p>" +
+    "<p>CO2 emissions per population, 2017: <strong>" + feature.properties.emissionsPerPopulation2017 + " kg</strong></p>" +
+    "<p>Difference 2010/2017:  <h5><strong>" + feature.properties.percentDifferenceEmission + "%</strong></h5></p>");
 }
         //Function for marker and legend colors
        
   function getColor(d) {
-    return d > 30 ? "#de2d26" : 
-    d > 20 ? "#e34a33" : 
-    d > 10 ? "#fdbb84" : 
-    d > 0 ? "#fee8c8" : 
-    d > -10 ? "#e5f5f9" : 
-    d > -20 ? "#99d8c9" : 
-    d < -20 ? "#2ca25f" : 
+    return d > 30 ? "#a63603" : 
+    d > 20 ? "#e6550d" : 
+    d > 10 ? "#fd8d3c" : 
+    d > 0 ? "#fdbe85" : 
+    d > -10 ? "#66c2a4" : 
+    d > -20 ? "#2ca25f" : 
+    d < -20 ? "#006d2c" : 
+    
     "#FFF";
 }
            
@@ -85,7 +89,7 @@ function createPopups(feature, layer) {
   }
 
   //Layer for markers-circles and pop-ups
-  var earthquakes = L.geoJSON(geojson.features, {
+  var cities = L.geoJSON(geojson.features, {
     onEachFeature: createPopups,
     pointToLayer: createCircles
   });
@@ -96,7 +100,7 @@ function createPopups(feature, layer) {
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [darkmap, earthquakes]
+    layers: [darkmap, cities]
   });
 
   //Creating a legend
@@ -110,9 +114,9 @@ function createPopups(feature, layer) {
 
     for (var i = 0; i < grades.length; i++) {
       from = grades[i];
-      to = grades[i + 1] - 1;
+      to = grades[i + 1];
       labels.push(
-        '<i style="background:' + getColor(from + 1) + '"></i> ' +
+        '<i style="background:' + getColor(from+1) + '"></i> ' +
         from + (to ? '&ndash;' + to : '+'));
     }
     
